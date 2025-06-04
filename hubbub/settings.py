d69bab9c9f282c4e349bf6019acf928b27c649ca
Rory Swietlicki <rory@swietlicki.eu>
2025-06-04 09:53:46 +0100

    Added questions model

--- hubbub/settings.py
+++ hubbub/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
-    'events'
+    'events',
+    'questions'
 ]
 
 AUTH_USER_MODEL = 'users.User'
---
+++ questions/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Questions(models.Model):
+    event = models.ForeignKey(
+        to='events.Event',
+        related_name='questions',
+        on_delete=models.CASCADE
+    )
+    question = models.TextField(max_length=1000)
+    response = models.TextField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.question  
