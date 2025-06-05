8e63de612d1cc88280b0f5c80b02f662b9a8ec18
Rory Swietlicki <rory@swietlicki.eu>
2025-06-05 15:39:43 +0100

    Fixed custom token serializer

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView
 
-from .views import SignUpView, ProfileView
+from .views import SignUpView, ProfileView, CustomTokenObtainPairView
 
 
 urlpatterns = [
     path('sign-up/', SignUpView.as_view()),
-    path('sign-in/', TokenObtainPairView.as_view()),
+    path('sign-in/', CustomTokenObtainPairView.as_view()),
     path('profile/', ProfileView.as_view())
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .serializers.common import UserSerializer
 from .serializers.populated import ProfileSerializer
+from .serializers.tokens import CustomTokenSerializer
 
 
 # Create your views here.
@@ -13,6 +15,10 @@ class SignUpView(APIView):
         serialized_user.is_valid(raise_exception=True)
         serialized_user.save()
         return Response({ f'detail': 'Sign up successful' })
+    
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenSerializer
 
 
 # Profile view
