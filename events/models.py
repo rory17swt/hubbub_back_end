555708d492009ce1d9acfbdc5296d899edf0e02a
Rory Swietlicki <rory@swietlicki.eu>
2025-06-10 16:44:03 +0100

    image filed added to event model

--- events/models.py
+++ events/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from cloudinary_storage.storage import MediaCloudinaryStorage
 
 # Create your models here.
 class Event(models.Model):
@@ -15,7 +16,7 @@ class Event(models.Model):
     contact_email = models.EmailField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/', blank=True, storage=MediaCloudinaryStorage)
 
     def __str__(self):
         return self.title
